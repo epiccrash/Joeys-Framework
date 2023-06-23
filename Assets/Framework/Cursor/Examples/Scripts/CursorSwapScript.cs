44254cb401b78af7c1089c59b87b281200f67921
Joey Perrino <epiccrashv3@gmail.com>
2023-06-22 20:10:12 -0400

    Example scene for cursor use updated
    - More detailed panel
    - Hotkeys now display for hiding/showing the cursor and locking/unlocking the cursor

--- Assets/Framework/Cursor/Examples/Scripts/CursorSwapScript.cs
+++ Assets/Framework/Cursor/Examples/Scripts/CursorSwapScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace Framework.Cursor.Examples
 {
@@ -10,35 +11,103 @@ namespace Framework.Cursor.Examples
     public class CursorSwapScript : MonoBehaviour
     {
         /// <summary>
+        /// The KeyCode used to show the cursor.
+        /// </summary>
+        [Header("Cursor Visibility KeyCodes")]
+        [SerializeField]
+        private KeyCode showCursorKeyCode = KeyCode.S;
+        /// <summary>
+        /// The KeyCode used to hide the cursor.
+        /// </summary>
+        [SerializeField]
+        private KeyCode hideCursorKeyCode = KeyCode.H;
+
+        /// <summary>
+        /// The KeyCode used to free the cursor from a lock state.
+        /// </summary>
+        [Header("Cursor Lock KeyCodes")]
+        [SerializeField]
+        private KeyCode freeCursorKeyCode = KeyCode.F;
+        /// <summary>
+        /// The KeyCode used to lock the cursor to the center of the game window.
+        /// </summary>
+        [SerializeField]
+        private KeyCode lockCursorToCenterKeyCode = KeyCode.C;
+        /// <summary>
+        /// The KeyCode used to lock the cursor to the boundaries of the game window.
+        /// </summary>
+        [SerializeField]
+        private KeyCode lockCursorToBoundariesKeyCode = KeyCode.B;
+
+        /// <summary>
+        /// The TextMeshPro displaying the KeyCode used to show the cursor.
+        /// </summary>
+        [Header("UI References")]
+        [SerializeField]
+        private TextMeshProUGUI showCursorTMP;
+        /// <summary>
+        /// The TextMeshPro displaying the KeyCode used to hide the cursor.
+        /// </summary>
+        [SerializeField]
+        private TextMeshProUGUI hideCursorTMP;
+        /// <summary>
+        /// The TextMeshPro displaying the KeyCode used to free the cursor from a lock state.
+        /// </summary>
+        [SerializeField]
+        private TextMeshProUGUI freeCursorTMP;
+        /// <summary>
+        /// The TextMeshPro displaying the KeyCode used to lock the cursor to the center of the game window.
+        /// </summary>
+        [SerializeField]
+        private TextMeshProUGUI lockCursorToCenterTMP;
+        /// <summary>
+        /// The TextMeshPro displaying the KeyCode used to lock the cursor to the boundaries of the game window.
+        /// </summary>
+        [SerializeField]
+        private TextMeshProUGUI lockCursorToBoundariesTMP;
+
+        /// <summary>
+        /// Sets the TextMeshPro text based on the KeyCodes set above.
+        /// </summary>
+        private void Awake()
+        {
+            showCursorTMP.text = $"Show cursor - {showCursorKeyCode}";
+            hideCursorTMP.text = $"Hide cursor - {hideCursorKeyCode}";
+            freeCursorTMP.text = $"Free cursor from lock state - {freeCursorKeyCode}";
+            lockCursorToCenterTMP.text = $"Lock cursor to center - {lockCursorToCenterKeyCode}";
+            lockCursorToBoundariesTMP.text = $"Lock cursor to boundaries - {lockCursorToBoundariesKeyCode}";
+        }
+
+        /// <summary>
         /// Changes the cursor's lock state and/or visibility based on the key pressed.
         /// </summary>
         private void Update()
         {
-            // Free the cursor by pressing A
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                CursorControl.Instance.FreeCursor();
-            }
-            // Lock the cursor to the center of the game window by pressing B
-            else if (Input.GetKeyDown(KeyCode.B))
-            {
-                CursorControl.Instance.LockCursorToCenterOfGameWindow();
-            }
-            // Lock the cursor to the boundaries of the game window by pressing C
-            else if (Input.GetKeyDown(KeyCode.C))
-            {
-                CursorControl.Instance.LockCursorToBoundariesOfGameWindow();
-            }
-
             // Make the cursor visible
-            if (Input.GetKeyDown(KeyCode.V))
+            if (Input.GetKeyDown(showCursorKeyCode))
             {
                 CursorControl.Instance.ShowCursor();
             }
             // Hide the cursor
-            else if (Input.GetKeyDown(KeyCode.H))
+            else if (Input.GetKeyDown(hideCursorKeyCode))
             {
                 CursorControl.Instance.HideCursor();
+            }
+
+            // Free the cursor
+            if (Input.GetKeyDown(freeCursorKeyCode))
+            {
+                CursorControl.Instance.FreeCursor();
+            }
+            // Lock the cursor to the center of the game window
+            else if (Input.GetKeyDown(lockCursorToCenterKeyCode))
+            {
+                CursorControl.Instance.LockCursorToCenterOfGameWindow();
+            }
+            // Lock the cursor to the boundaries of the game window
+            else if (Input.GetKeyDown(lockCursorToBoundariesKeyCode))
+            {
+                CursorControl.Instance.LockCursorToBoundariesOfGameWindow();
             }
         }
 
